e4b6e49fd3910f96cbc146e7906a0e24deb13bfe
Nasser AHMED-MALEK <nahmed-m@e3r2p1.42.fr>
2015-12-10 11:23:37 +0100

    #fix 2

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nahmed-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 16:21:47 by nahmed-m          #+#    #+#             */
-/*   Updated: 2015/11/29 01:40:17 by nahmed-m         ###   ########.fr       */
+/*   Updated: 2015/12/10 11:10:36 by nahmed-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*temp;
 
-	temp = (unsigned char*)malloc(sizeof(*temp) * n);
+	if ((temp = (unsigned char*)malloc(sizeof(*temp) * n)) == NULL)
+		return (NULL);
 	ft_memcpy(temp, src, n);
 	ft_memcpy(dest, temp, n);
 	free(temp);
